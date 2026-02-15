518c7bfd5fb588be8397a532696fdf44361d4617
Ziad El-Moursy <ziadmelmoursy@gmail.com>
2026-02-14 22:27:55 -0800

    Update Simple_Pendulum.py
    Fixed issues with animation.

--- Simple_Pendulum.py
+++ Simple_Pendulum.py
@@ -1,15 +1,12 @@
-  # -*- coding: utf-8 -*-
 """
 Created on Tue Sep  9 17:58:16 2025
 
 @author: ziadm
 """
 
-# scientific and numerical imports
+# imports
 from scipy.integrate import solve_ivp
 import numpy as np
-
-# matplotlib imports
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
@@ -44,14 +41,6 @@ if __name__ == '__main__':
             variable = nonfloat_error_handling(variable, variable_name, units)
         return variable
     
-    # pendulum ODE function
-    def pendulum_ODE(t, y):
-        return (y[1], -g*np.sin(y[0])/length)
-    
-    # pendulum position function
-    def pendulum_position(angle):
-        return (length*np.sin(angle), -length*np.cos(angle))
-    
     # length input and error-handling
     length = input('Enter the length of your pendulum (in meters): ')
     length = nonfloat_error_handling(length, 'pendulum length', 'meters')
@@ -65,29 +54,32 @@ if __name__ == '__main__':
     # initial angular velocity input and error-handling
     theta_dot0 = input('\nEnter the initial angular velocity of your pendulum (in degrees per second): ')
     theta_dot0 = nonfloat_error_handling(theta_dot0, 'initial angular velocity', 'degrees per second')
-        
-    # convert deg to rad
+    
+    # convert initial values from deg to rad
     theta0 = np.deg2rad(theta0)
     theta_dot0 = np.deg2rad(theta_dot0)
     
-    # solve pendulum ODE
+    # pendulum ODE system and numerical solution
+    def pendulum_ODE(t, y):
+        return (y[1], -g*np.sin(y[0])/length)
+    
     time_span = (0, 10)
     y0 = (theta0, theta_dot0)
     solution = solve_ivp(pendulum_ODE, time_span, y0,
-                         t_eval=np.linspace(time_span[0],time_span[1],60*time_span[1]))
+                         t_eval=np.linspace(time_span[0],time_span[1],30*time_span[1]))
     
     '''
         Input ODE function, 1D independent variable range ("time_span"), 
     initial conditions vector ("y0": initial angular position and velocity).
     
-        "solve_ivp" links "t_span" to "t" and "y0" to "y", setting the return vector
-    of the ODE function (y[1], -g*np.sin(y[0])/length) to be the derivative of the
-    vector (y[0], y[1]) that corresponds to "y0", all with respect to "t".
+        "solve_ivp" links "t_span" to "t" and set setsthe return tuple of the
+    pendulum_ODE function (y[1], -g*np.sin(y[0])/length) to be the derivative with
+    respect to "t" of y = (y[0], y[1]), whose initial values are "y0".
     
-        Python now knows that "y[1]" is the derivative of "y[0]", and that
-    "-g*np.sin(y[0])/length" is the derivative of "y[1]". It thus deduces that
-    "-g*np.sin(y[0])/length" is the second derivative of "y[0]", and knows how to
-    evolve the system of ODEs from the initial conditions (theta0, theta_dot0).
+        SciPy now knows that "y[1]" is the derivative of "y[0]", and that
+    "-g*np.sin(y[0])/length" is the derivative of "y[1]". It logically deduces that
+    "-g*np.sin(y[0])/length" is the second derivative of "y[0]", and evolves
+    the system of ODEs from the initial conditions (theta0, theta_dot0).
     
         "np.linspace(a,b,c)" tells "solve_ivp" to generate c equally spaced intervals
     from a to b, so c = fps*(b-a). As "np.linspace" is passed into the "t_eval" parameter,
@@ -95,13 +87,13 @@ if __name__ == '__main__':
     appended into the array "t". Each iterated angular position and velocity is respectively
     appended into columns "y[0]" and "y[1]" of the array "y". Arrays are appended into by row.
     '''
-    
+      
     # store solution results
     time = solution.t
     theta = solution.y[0]
     theta_dot = solution.y[1]
     
-    # convert rad to degrees
+    # convert solution from rad to degrees
     theta = np.rad2deg(solution.y[0])
     theta_dot = np.rad2deg(solution.y[1])
     
@@ -133,6 +125,8 @@ if __name__ == '__main__':
     plt.grid()
     plt.show()
     
+    ###---ANIMATION---###
+    
     # animate time domain of angular position and velocity
     fig, ax = plt.subplots()
     
@@ -147,14 +141,25 @@ if __name__ == '__main__':
     ax.legend([r'$\theta$ [deg]', r'$\dot \theta$ [deg/s]'])
     ax.grid()
     
+    def init_time():
+        theta_curve.set_data([], [])
+        theta_dot_curve.set_data([], [])
+        return theta_curve, theta_dot_curve
+    
     def animate_time_domain(i):
         theta_curve.set_data(time[:i+1], theta[:i+1])
         theta_dot_curve.set_data(time[:i+1], theta_dot[:i+1])
+        return theta_curve, theta_dot_curve
     
     animation_time_domain = animation.FuncAnimation(fig, animate_time_domain,
-                                                    frames=len(time))
-    ffmpeg_writer = animation.FFMpegWriter(fps=60)
+                                                    frames=range(len(time)),
+                                                    init_func=init_time,
+                                                    blit=True)
+    
+    ffmpeg_writer = animation.FFMpegWriter(fps=30)
     animation_time_domain.save('SP_time_domain.mp4', writer=ffmpeg_writer)
+    
+    plt.close(fig)
     
     # animate phase diagram of angular position and velocity
     fig, ax = plt.subplots()
@@ -169,16 +174,33 @@ if __name__ == '__main__':
     ax.set_ylabel(r'$\dot \theta$ [deg/s]')
     ax.grid()
     
+    def init_phase():
+        phase_curve.set_data([], [])
+        phase_point.set_data([], [])
+        return phase_curve, phase_point
+    
     def animate_phase_diagram(i):
         phase_curve.set_data(theta[:i+1], theta_dot[:i+1])
         phase_point.set_data([theta[i]], [theta_dot[i]])
+        return phase_curve, phase_point
     
     animation_phase_diagram = animation.FuncAnimation(fig, animate_phase_diagram,
-                                                      frames=len(time))
-    ffmpeg_writer = animation.FFMpegWriter(fps=60)
+                                                      frames=range(len(time)),
+                                                      init_func=init_phase,
+                                                      blit=True)
+    
+    ffmpeg_writer = animation.FFMpegWriter(fps=30)
     animation_phase_diagram.save('SP_phase_diagram.mp4', writer=ffmpeg_writer)
     
+    plt.close(fig)
+    
     # animate pendulum motion
+    def pendulum_position(angle):
+        return (length*np.sin(angle), -length*np.cos(angle))
+    
+    theta = np.deg2rad(theta)
+    theta_dot = np.deg2rad(theta_dot)
+    
     fig = plt.figure()
     ax = fig.add_subplot(aspect='equal')
     ax.set_xlim(-length, length)
@@ -189,12 +211,15 @@ if __name__ == '__main__':
     line, = ax.plot([0, x0], [0, y0], lw=2, c='k')
     circle = ax.add_patch(plt.Circle(pendulum_position(theta0), 0.05, fc='r', zorder=3))
     
-    def animate_pendulum(i):
+    def animate_motion(i):
         x, y = pendulum_position(theta[i])
         line.set_data([0, x], [0, y])
         circle.set_center((x, y))
     
-    animation_pendulum = animation.FuncAnimation(fig, animate_pendulum,
-                                                 frames=len(time))
-    ffmpeg_writer = animation.FFMpegWriter(fps=60)
-    animation_pendulum.save('SP_animation.mp4', writer=ffmpeg_writer)
+    animation_motion = animation.FuncAnimation(fig, animate_motion,
+                                                 frames=range(len(time)))
+    
+    ffmpeg_writer = animation.FFMpegWriter(fps=30)
+    animation_motion.save('SP_motion.mp4', writer=ffmpeg_writer)
+    
+    plt.close(fig)
